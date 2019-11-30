fc8f6cab522656ccba3787d2fea04eecded61903
vladimircordas <tt_cox@hotmail.com>
2019-11-30 16:50:19 +0100

    Updates

---
+++ precache-manifest.0a606153698567d346cc8ee35a79b1a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb658a3e9ca5cd14701c35e41a82dd3d",
+    "url": "/darksky-weatherapp/index.html"
+  },
+  {
+    "revision": "27a14fe53131e50134a9",
+    "url": "/darksky-weatherapp/static/css/main.931882a7.chunk.css"
+  },
+  {
+    "revision": "63792aa8db7e85af1541",
+    "url": "/darksky-weatherapp/static/js/2.1aa5e60f.chunk.js"
+  },
+  {
+    "revision": "27a14fe53131e50134a9",
+    "url": "/darksky-weatherapp/static/js/main.ba3b199d.chunk.js"
+  },
+  {
+    "revision": "f64b4f2d5e1f61529430",
+    "url": "/darksky-weatherapp/static/js/runtime-main.63660228.js"
+  },
+  {
+    "revision": "501201c3f7e543fecef6f691dab40efa",
+    "url": "/darksky-weatherapp/static/media/checkmark.501201c3.svg"
+  },
+  {
+    "revision": "7c7ebaa467cd472a2db2f75ee6d9a7c5",
+    "url": "/darksky-weatherapp/static/media/options.7c7ebaa4.svg"
+  }
+]);
